ec3b851eb97e2d3fca3b8102e0ff3dbe7df3d28c
Michael Berube <beru0038@algonquinlive.com>
2021-08-07 12:16:08 -0400

    wip of Bloodbank logic

--- src/main/java/logic/LogicFactory.java
+++ src/main/java/logic/LogicFactory.java
@@ -5,7 +5,9 @@ public abstract class LogicFactory {
     private static final String PACKAGE = "logic.";
     private static final String SUFFIX = "Logic";
 
+    
     private LogicFactory() {
+        
     }
 
     //TODO this code is not complete, it is just here for sake of programe working. need to be changed ocmpletely
