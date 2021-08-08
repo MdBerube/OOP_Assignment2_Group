984e6bfebd9bc1750ccb49f5c620d210bf172f7d
Michael Berube <beru0038@algonquinlive.com>
2021-08-08 16:03:56 -0400

    finished logic factory? will require exception catches further up

--- src/main/java/logic/LogicFactory.java
+++ src/main/java/logic/LogicFactory.java
@@ -1,18 +1,44 @@
 package logic;
 
+import java.lang.ClassNotFoundException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public abstract class LogicFactory {
 
     private static final String PACKAGE = "logic.";
     private static final String SUFFIX = "Logic";
-
-    
+  
+    // main factory implementation?
     private LogicFactory() {
         
     }
-
-    //TODO this code is not complete, it is just here for sake of program working. need to be changed completely
-    public static < T> T getFor( String entityName ) {
-        //this casting wont be needed.
-        return (T)new AccountLogic();
+    
+    public static < T> T getFor( String entityName ) throws ClassNotFoundException {
+        
+         T newInstance = null;
+         Class<T> type;
+           try {
+               type = (Class<T>) Class.forName(PACKAGE + entityName + SUFFIX);
+               newInstance = getFor(type);
+           }catch (Exception e){
+               e.printStackTrace();
+           }
+           return newInstance; 
     }
+    
+    public static<T> T getFor(Class type) throws InstantiationException , IllegalAccessException,  
+    IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+        
+        T newInstance = null;
+        try{
+            // getDeclared returns a constructor 
+            Constructor<T> declaredConstructor = (type.getDeclaredConstructor());
+            newInstance = declaredConstructor.newInstance();
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
+        return newInstance;
+    }
+  
 }
